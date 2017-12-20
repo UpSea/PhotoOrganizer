dad22517001808e174b038f54a6c3835440c7b4e
Lucas McNinch <lcmcninch@yahoo.com>
2017-12-19 20:10:57 -0500

    Make all thumbnails square.

--- src/ImageMan.py
+++ src/ImageMan.py
@@ -1,19 +1,26 @@
 from PyQt4 import QtGui
-from PIL import Image
+from PIL import Image, ImageOps
 from io import BytesIO
 
 
 def getThumbnailIcon(filePath, size=200):
     if isinstance(filePath, basestring):
         im = Image.open(filePath)
-        im.thumbnail((size, size))
+        thumb = ImageOps.fit(im, (size, size), Image.ANTIALIAS)
     elif isinstance(filePath, Image.Image):
-        im = filePath.copy()
+        thumb = filePath.copy()
     else:
         raise TypeError('FILEPATH must be an Image or path to an image file')
 
     fp = BytesIO()
-    im.save(fp, 'png')
+    thumb.save(fp, 'png')
     pix = QtGui.QPixmap()
     pix.loadFromData(fp.getvalue())
     return QtGui.QIcon(pix)
+
+if __name__ == "__main__":
+    filePath = r"C:\Users\Luke\Desktop\Pictures\Screenshot.jpg"
+    image = Image.open(filePath)
+    size = (200, 200)
+    thumb = ImageOps.fit(image, size, Image.ANTIALIAS)
+    thumb.show()
